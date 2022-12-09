eb25065d11d721d82fb2ae9d8ba181b09df1ac63
Neville Rabakale <neville.rabakale@gmail.com>
2022-12-09 23:20:42 +0100

    Add project files.

---
+++ BandMvc/Controllers/BandsController.cs
@@ -0,0 +1,29 @@
+﻿using BandMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BandMvc.Controllers
+{
+    public class BandsController : Controller
+    {
+        DataService service;
+        public BandsController()
+        {
+            service= new DataService();
+        }
+        [HttpGet("")]
+        [HttpGet("index")]
+        public IActionResult Index()
+        {
+            var model = service.GetAlBands();
+            return View(model);
+        }
+
+        [HttpGet("details/{id}")]
+        public IActionResult Details(int id)
+        {
+            var model = service.GetBandById(id);
+            return View(model);
+        }
+
+    }
+}
---
+++ BandMvc/Models/DataService.cs
@@ -0,0 +1,22 @@
+﻿namespace BandMvc.Models
+{
+    public class DataService
+    {
+        static List<Band> bands= new List<Band>()
+        {
+            new Band{ Id= 0, Name = "System of a Down", Description = "System of a Down is an Armenian American heavy metal band formed in Glendale, California, in 1994. Since 1997, the band has consisted of Serj Tankian; Daron Malakian; Shavo Odadjian; and John Dolmayan, who replaced original drummer Andy Khachaturian" },
+            new Band{ Id= 1, Name = "Bullet for my valentine", Description = "Bullet for My Valentine, often abbreviated as BFMV, are a Welsh heavy metal band from Bridgend, formed in 1998. The band is currently composed of Matthew Tuck, Michael Paget, Jason Bowld and Jamie Mathias. Former members include Michael Thomas, Jason James and Nick Crandle; the latter were on bass." },
+            new Band{ Id= 2, Name = "Likin Park", Description = "Linkin Park is an American rock band from Agoura Hills, California. The band's current lineup comprises vocalist/rhythm guitarist/keyboardist Mike Shinoda, lead guitarist Brad Delson, bassist Dave Farrell, DJ/turntablist Joe Hahn and drummer Rob Bourdon, all of whom are founding members" },
+        };
+
+        public Band GetBandById(int id) 
+        {
+            return bands.Single(o => o.Id == id); 
+        }
+
+        public Band[] GetAlBands()
+        {
+            return bands.ToArray();
+        }
+    }
+}
